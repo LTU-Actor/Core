5675952f406f8858a64b62fef19aaac7782c0cb4
Mitchell Pleune <mitchpleune@gmail.com>
2019-09-21 10:37:47 -0400

    Run npm commands inside web_control script
    I think this works out nicer than when running catkin build.
    npm install works fine without internet as long as all the dependicies
    are already fetched.

--- src/web_control.py
+++ src/web_control.py
@@ -4,6 +4,7 @@ import rospy
 import rospkg
 import psutil
 import os
+import sys
 
 rospy.init_node('web_control')
 rospack = rospkg.RosPack()
@@ -19,10 +20,24 @@ web_env['VIDEO2'] = str(rospy.get_param('~video2'))
 web_env['VIDEO3'] = str(rospy.get_param('~video3'))
 web_env['VIDEO4'] = str(rospy.get_param('~video4'))
 web_env['ROS_NS'] = str(rospy.get_namespace())
+#web_env['NPM_CONFIG_PREFIX'] = '/some/path/to/node_modules'
 web_cwd = rospack.get_path('ltu_actor_core') + '/web'
+
+rospy.init_node('web_control')
+
 if 'ACTOR_CORE_WEB_HOTBUILD' in web_env.keys():
     web_server = psutil.Popen('npm run dev', shell=True, cwd=web_cwd, env=web_env)
 else:
+    web_install = psutil.Popen('npm install', shell=True, cwd=web_cwd, env=web_env)
+    if web_install.wait(timeout=60) is None:
+        web_install.terminate()
+        rospy.logfatal('Failed to finish npm install')
+
+    web_build = psutil.Popen('npm run build', shell=True, cwd=web_cwd, env=web_env)
+    if web_build.wait(timeout=60) is None:
+        web_build.terminate()
+        rospy.logfatal('Failed to finish npm run build')
+
     web_server = psutil.Popen('npm run start', shell=True, cwd=web_cwd, env=web_env)
 
 def on_shutdown():
